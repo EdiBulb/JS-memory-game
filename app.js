9666df972ac2027cc032e36890a253bad2ae7d47
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-06-03 19:56:16 +1200

    Parsing JSON to use, Create 12 cards

--- app.js
+++ app.js
@@ -2,11 +2,37 @@
 // let 키워드로 변수를 정의한다.
 let cards = [];
 
-LoadCards();
 
 
 // implement the Fetch API to grab the card JSON file
+fetch("./data/card_info.json")
+    .then(Response => Response.json())
+    .then((data) => {
+        // Option 1 using MAP
+        // const cardsWithMap = data.map(card => [card, card]).flat();
+        // console.log(cardsWithMap);
 
+        // Option 2 using flatmap()
+        // const cardsWithFlatMap = data.flatMap(card => {
+        //     return [card, card];
+        // })
+        // console.log(cardsWithFlatMap);
+
+        // Option 3 (easiest)
+        cards = [...data, ...data];
+        console.log(cards);
+
+
+    })
+    .catch((error) => {
+        console.log("Error fetching card data: ", error)
+    })
+
+
+/** Loadcards()
+*/
+
+/** 
 async function LoadCards() {
     try{
         // fetch the JSON file, fetch는 요청을 보낼 때 쓴다. // await: response가 오길 기다려야한다. 
@@ -21,3 +47,4 @@ async function LoadCards() {
         console.log(error);
     }
 };
+*/
