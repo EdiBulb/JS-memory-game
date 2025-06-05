b7d96964a1825dd9fd4a210e7795bd6dbb596bad
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-06-05 17:30:46 +1200

    Check if cards are matched
    - setTimeout
    - remove CSS to unflip cards.

--- app.js
+++ app.js
@@ -38,146 +38,168 @@ fetch("./data/card_info.json") // fetch() 파일을 요청하는 함수이다. f
     }
 );// end fetch
 
-    // define shuffle function
-    function shuffle(){
-        // create a copy of the cards array to avoid mutating the original array
-        let shuffledCardsArray = [...cards];
-        let totalCards = shuffledCardsArray.length;
-        let currentIndex = totalCards - 1;
 
-        // use Fisher-Yates (or Knuth) shuffle algorithm. This method is efficient and ensures that each possible permutation of the array has an equal probability of occurring.
+// define shuffle function
+function shuffle(){
+    // create a copy of the cards array to avoid mutating the original array
+    let shuffledCardsArray = [...cards];
+    let totalCards = shuffledCardsArray.length;
+    let currentIndex = totalCards - 1;
+
+    // use Fisher-Yates (or Knuth) shuffle algorithm. This method is efficient and ensures that each possible permutation of the array has an equal probability of occurring.
+    
+    /** 
+    // option 1
+    // Loop through the array from the last element to the first
+    
+    for(currentIndex; currentIndex > 0; currentIndex--) {
+        // Generate a random index between 0 and currentIndex (inclusive)
+        let randomCardIndex = Math.floor(Math.random() * (currentIndex + 1));
+        console.log("randomCardIndex: ", randomCardIndex);
+
+        // Swap the elements at currentIndex and randomIndex using a temporary variable
+        let randomCard = shuffledCardsArray[randomCardIndex];
+        console.log("randomCard: ", randomCard);
+
+        // replace the randomCard with the card at the currentIndex
+        shuffledCardsArray[randomCardIndex] = shuffledCardsArray[currentIndex];
+        console.log("shuffledCardsArrayStep1: ", [...shuffledCardsArray]);
+
+        // replace the card at currentIndex with the randomCard
+        shuffledCardsArray[currentIndex] = randomCard;
+        console.log("shuffledCardsArrayStep2: ", [...shuffledCardsArray]);
+
+    }
+    */
+
+    // option 2
+    // Swap elements using destructuring assignment in JavaScript
+    for(currentIndex; currentIndex > 0; currentIndex--) {
+        // Generate a random index between 0 and currentIndex (inclusive)
+        let randomCardIndex = Math.floor(Math.random() * (currentIndex + 1));
+        [shuffledCardsArray[currentIndex], shuffledCardsArray[randomCardIndex]] = [shuffledCardsArray[randomCardIndex], shuffledCardsArray[currentIndex]];
+
+    }
         
-        /** 
-        // option 1
-        // Loop through the array from the last element to the first
+    return shuffledCardsArray;
+
+}; //  end shuffle // 이렇게 각 괄호가 끝나는 부분에, 의미하는 것을 적어두면 알아보기 쉽겠다. 습관으로 만들자.
+
+function dealCards(cards) {
+    console.log('welcome to the random card game')
+
+    let fragment = document.createDocumentFragment(); //  document.createDocumentFragment() returns a minimal document object with no parent
         
-        for(currentIndex; currentIndex > 0; currentIndex--) {
-            // Generate a random index between 0 and currentIndex (inclusive)
-            let randomCardIndex = Math.floor(Math.random() * (currentIndex + 1));
-            console.log("randomCardIndex: ", randomCardIndex);
 
-            // Swap the elements at currentIndex and randomIndex using a temporary variable
-            let randomCard = shuffledCardsArray[randomCardIndex];
-            console.log("randomCard: ", randomCard);
+    // cards 배열을 for of 반복문으로 순회한다.
+    for (const card of cards) {
+        // OPTION 1: directly adding created elements to the DOM
+        // #1. create the card wrapper, 문서 객체를 생성한다.
+        // let cardElement = document.createElement("div"); // document 객체를 써서 DOM에 접근한다.
+        // cardElement.classList.add("card"); // 클래스를 부여한다.
+        // cardElement.setAttribute("data-name", card.name); // 속성을 부여한다.
 
-            // replace the randomCard with the card at the currentIndex
-            shuffledCardsArray[randomCardIndex] = shuffledCardsArray[currentIndex];
-            console.log("shuffledCardsArrayStep1: ", [...shuffledCardsArray]);
+        // // #2. add the front and back of the card
+        // cardElement.innerHTML = `
+        //     <div class="back">
+        //         <img class="back-image" src="${card.image}.png">
+        //     </div>
+        //     <div class="front"></div>
+        // `;
 
-            // replace the card at currentIndex with the randomCard
-            shuffledCardsArray[currentIndex] = randomCard;
-            console.log("shuffledCardsArrayStep2: ", [...shuffledCardsArray]);
+        // // 카드를 테이블에 연결한다.
+        // cardTable.appendChild(cardElement);
 
-        }
-        */
+        // Option 2: using fragments.
+        // create our entire card
+        // div class = card 요소를 새로 만든다.
+        let cardElement = document.createElement("div"); 
+        cardElement.classList.add("card");
+        cardElement.setAttribute("data-name", card.name);
 
-        // option 2
-        // Swap elements using destructuring assignment in JavaScript
-        for(currentIndex; currentIndex > 0; currentIndex--) {
-            // Generate a random index between 0 and currentIndex (inclusive)
-            let randomCardIndex = Math.floor(Math.random() * (currentIndex + 1));
-            [shuffledCardsArray[currentIndex], shuffledCardsArray[randomCardIndex]] = [shuffledCardsArray[randomCardIndex], shuffledCardsArray[currentIndex]];
+        // create both the front and back of the cards, seperately
+        // Front of card
+        // 카드 front 영역 div만든다.
+        let frontCardDiv = document.createElement("div");
+        frontCardDiv.classList.add("front");
 
-        }
-         
-        return shuffledCardsArray;
+        // Back of card
+        // 카드 back 영역 div 만든다.
+        let backCardDiv = document.createElement("div");
+        backCardDiv.classList.add("back");
 
-    }; //  end shuffle // 이렇게 각 괄호가 끝나는 부분에, 의미하는 것을 적어두면 알아보기 쉽겠다. 습관으로 만들자.
+        // add image to the back of the card
+        // 카드 back에 들어갈 img 태그 만든다.
+        let img = document.createElement("img");
+        img.classList.add("back-image");
+        img.src = `${card.image}.png`;
+        // 만든 img를 backCardDiv에 붙인다.
+        backCardDiv.appendChild(img);
 
-    function dealCards(cards) {
-        console.log('welcome to the random card game')
+        // append front and back of the card, to the card itself.
+        // 만든 cardElement안에 front와 back 모두 붙인다.
+        cardElement.append(backCardDiv,frontCardDiv); // 먼저 불린 순서로 카드가 쌓여서, 커버가 보이게 하려면 front를 나중에 불러야한다.
+        
+        // attach card to the fragment
+        // 최종적으로 만든 cardElement를 fragment 안에 담아둔다.
+        fragment.appendChild(cardElement);
 
-        let fragment = document.createDocumentFragment(); //  document.createDocumentFragment() returns a minimal document object with no parent
-            
+    } // end of the for loop
 
-        // cards 배열을 for of 반복문으로 순회한다.
-        for (const card of cards) {
-            // OPTION 1: directly adding created elements to the DOM
-            // #1. create the card wrapper, 문서 객체를 생성한다.
-            // let cardElement = document.createElement("div"); // document 객체를 써서 DOM에 접근한다.
-            // cardElement.classList.add("card"); // 클래스를 부여한다.
-            // cardElement.setAttribute("data-name", card.name); // 속성을 부여한다.
+    // append the entire fragment to the live DOM
+    // fragment 에 담긴 모든 카드 요소를 한 번에 실제 DOM에 붙인다.
+    cardTable.appendChild(fragment);
 
-            // // #2. add the front and back of the card
-            // cardElement.innerHTML = `
-            //     <div class="back">
-            //         <img class="back-image" src="${card.image}.png">
-            //     </div>
-            //     <div class="front"></div>
-            // `;
+    // Attach click event listeners after all cards are added
+    let dealtCards = document.querySelectorAll('.card'); // 12개 카드를 모두 가져온다.
+    // forEach: 배열을 순회회
+    dealtCards.forEach(card => {
 
-            // // 카드를 테이블에 연결한다.
-            // cardTable.appendChild(cardElement);
+        card.addEventListener("click", flipCard);
+    });
+}; // end dealCards
 
-            // Option 2: using fragments.
-            // create our entire card
-            // div class = card 요소를 새로 만든다.
-            let cardElement = document.createElement("div"); 
-            cardElement.classList.add("card");
-            cardElement.setAttribute("data-name", card.name);
+function flipCard() {
+    if(noFlipping) return; //  카드 2개 flip한 뒤 3번째 하려고 하면 함수 자체를 나간다. 
 
-            // create both the front and back of the cards, seperately
-            // Front of card
-            // 카드 front 영역 div만든다.
-            let frontCardDiv = document.createElement("div");
-            frontCardDiv.classList.add("front");
+    // add a css class to activate the flip effect
+    this.classList.add("flipped"); // 클래스를 부여한다.
 
-            // Back of card
-            // 카드 back 영역 div 만든다.
-            let backCardDiv = document.createElement("div");
-            backCardDiv.classList.add("back");
+    // grab first card flipped over (clicked)
+    if(!firstCard) {
+        firstCard = this; // set the firstCard value to the div with class "card"
+        console.log("firstCard: ", firstCard);
+        return; // exit out of this flipCard function and wait for user to flip another card
+    }
 
-            // add image to the back of the card
-            // 카드 back에 들어갈 img 태그 만든다.
-            let img = document.createElement("img");
-            img.classList.add("back-image");
-            img.src = `${card.image}.png`;
-            // 만든 img를 backCardDiv에 붙인다.
-            backCardDiv.appendChild(img);
+    secondCard = this;
+    console.log("secondCard:", secondCard);
 
-            // append front and back of the card, to the card itself.
-            // 만든 cardElement안에 front와 back 모두 붙인다.
-            cardElement.append(backCardDiv,frontCardDiv); // 먼저 불린 순서로 카드가 쌓여서, 커버가 보이게 하려면 front를 나중에 불러야한다.
-            
-            // attach card to the fragment
-            // 최종적으로 만든 cardElement를 fragment 안에 담아둔다.
-            fragment.appendChild(cardElement);
+    noFlipping = true; // prevent user from clicking on more than 2 cards at once. 
 
-        } // end of the for loop
+    checkForMatch();
 
-        // append the entire fragment to the live DOM
-        // fragment 에 담긴 모든 카드 요소를 한 번에 실제 DOM에 붙인다.
-        cardTable.appendChild(fragment);
+};// end filpCard
 
-        // Attach click event listeners after all cards are added
-        let dealtCards = document.querySelectorAll('.card'); // 12개 카드를 모두 가져온다.
-        // forEach: 배열을 순회회
-        dealtCards.forEach(card => {
+function checkForMatch() {
+    let isMatch = (firstCard.dataset.name === secondCard.dataset.name); // boolean, indicating whether i have a match
+    
+    // the ternary operator in JavaScript
+    isMatch ? matchCards() : unflipCards();
 
-            card.addEventListener("click", flipCard);
-        });
-    }; // end dealCards
+}; // end checkForMatch
 
-    function flipCard() {
-        if(noFlipping) return; //  카드 2개 flip한 뒤 3번째 하려고 하면 함수 자체를 나간다. 
-
-        // add a css class to activate the flip effect
-        this.classList.add("flipped"); // 클래스를 부여한다.
-
-        // grab first card flipped over (clicked)
-        if(!firstCard) {
-            firstCard = this; // set the firstCard value to the div with class "card"
-            console.log("firstCard: ", firstCard);
-            return; // exit out of this flipCard function and wait for user to flip another card
-        }
-
-        secondCard = this;
-        console.log("secondCard:", secondCard);
-
-        noFlipping = true; // prevent user from clicking on more than 2 cards at once. 
+function unflipCards() {
+    setTimeout(() => {
+        // 뒤집는 CSS를 없애준다.
+        firstCard.classList.remove("flipped");  
+        secondCard.classList.remove("flipped");
+    }, 1000);
+    
+    
+}; // end unflipCards
 
 
-    }// end filpCard
 
 
 /** Loadcards()
